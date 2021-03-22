0fd7c2826b02e94e8ac2dec9e7f83e15f1f9b47d
Ronny Ohawa <ochiengohawa@gmail.com>
2021-03-22 22:39:19 +0300

    third commit

---
+++ arithmetic  operators.py
@@ -0,0 +1,24 @@
+a=10
+b=5
+c=0
+c = a+b
+print("value of c is",c)
+c= a-b
+print("value of c is",c)
+c=a*b
+print("value of c is",c)
+c=a/b
+print("value of c is",c)
+c=a%b
+print("value of c is",c)
+c=a**b
+print("value of c is",c)
+a=30
+b=25
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+
+
